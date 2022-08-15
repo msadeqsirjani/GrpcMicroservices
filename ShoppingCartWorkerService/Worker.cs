5ffd765178bbef918121b94414b80f41b1e5fdcc
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2022-08-15 23:26:30 +0430

    complete workload of adding product into cart

--- ShoppingCartWorkerService/Worker.cs
+++ ShoppingCartWorkerService/Worker.cs
@@ -15,33 +15,86 @@ public class Worker : BackgroundService
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            // create shopping cart ✅
-            var cart = await CreateShoppingCart();
+            const int maxRetryAttempts = 10;
+            var pauseBetweenFailures = TimeSpan.FromSeconds(3);
 
-            Console.WriteLine(cart);
-            
-            // fetch products from product gRPC with server stream
-            // add item into cart with client stream
+            await Policy
+                .Handle<RpcException>()
+                .WaitAndRetryAsync(maxRetryAttempts, _ => pauseBetweenFailures, (exception, pause) =>
+                {
+                    Console.WriteLine($"{exception.Message} => {pause.TotalSeconds}");
+                })
+                .ExecuteAsync(async () => await Do(stoppingToken));
+
+            await Do(stoppingToken);
 
             await Task.Delay(_options.Interval, stoppingToken);
         }
     }
 
-    private async Task<GetShoppingCartResponse> CreateShoppingCart()
+    private async Task Do(CancellationToken stoppingToken)
     {
         using var cartChannel = GrpcChannel.ForAddress(_options.ShoppingCartUrl);
         var cartClient = new ShoppingCartService.ShoppingCartServiceClient(cartChannel);
 
-        await cartClient.CreateShoppingCartAsync(new CreateShoppingCartRequest()
+        await CreateShoppingCart(cartClient);
+
+        using var cartStream = cartClient.CreateShoppingCartItem();
+
+        using var productChannel = GrpcChannel.ForAddress(_options.ProductUrl);
+        var productClient = new ProductService.ProductServiceClient(productChannel);
+        using var products = productClient.GetProducts(new GetProductsRequest
+        {
+            PageNo = 1,
+            PageSize = 100
+        });
+
+        var discountCodes = new[]
+        {
+            "E3CE571D",
+            "EE5ECF60",
+            "8E8EEA24"
+        };
+
+        var colors = new[]
+        {
+            "Black",
+            "White",
+            "Red",
+            "Green"
+        };
+
+        await foreach (var product in products.ResponseStream.ReadAllAsync(stoppingToken))
+        {
+            var item = new CreateShoppingCartItemRequest
+            {
+                Username = _options.Username,
+                DiscountCode = discountCodes[Random.Shared.Next(discountCodes.Length)],
+                Item = new ShoppingCartItemRequest
+                {
+                    ProductId = product.Id,
+                    ProductName = product.Title ?? "Car",
+                    Price = product.Price,
+                    Color = colors[Random.Shared.Next(colors.Length)],
+                    Quantity = 1,
+                }
+            };
+
+            await cartStream.RequestStream.WriteAsync(item, stoppingToken);
+        }
+
+        await cartStream.RequestStream.CompleteAsync();
+
+        var itemResponse = await cartStream;
+
+        Console.WriteLine(itemResponse);
+    }
+
+    private async Task CreateShoppingCart(ShoppingCartService.ShoppingCartServiceClient client)
+    {
+        await client.CreateShoppingCartAsync(new CreateShoppingCartRequest
         {
             Username = _options.Username
         });
-
-        var cartResponse = await cartClient.GetShoppingCartAsync(new GetShoppingCartRequest()
-        {
-            Username = _options.Username
-        });
-
-        return cartResponse;
     }
 }
