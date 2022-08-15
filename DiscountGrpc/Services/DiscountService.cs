890edb934906ba9f2b33dcdd66e6f4712ebe5e50
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2022-08-15 20:16:06 +0430

    add discount gRPC service

---
+++ DiscountGrpc/Services/DiscountService.cs
@@ -0,0 +1,25 @@
+﻿namespace DiscountGrpc.Services;
+
+public class DiscountService : Protos.DiscountService.DiscountServiceBase
+{
+    private readonly DiscountDbContext _context;
+
+    public DiscountService(DiscountDbContext context)
+    {
+        _context = context;
+    }
+
+    public override Task<Empty> Available(Empty request, ServerCallContext context)
+    {
+        return Task.FromResult(new Empty());
+    }
+
+    public override Task<GetDiscountResponse?> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        var discount = _context.Discounts.FirstOrDefault(x => x.Code == request.Discount);
+
+        var discountResponse = discount?.Adapt<GetDiscountResponse>();
+
+        return Task.FromResult(discountResponse);
+    }
+}
--- DiscountGrpc/Usings.cs
+++ DiscountGrpc/Usings.cs
@@ -2,3 +2,7 @@
 global using DiscountGrpc.Data;
 global using DiscountGrpc.Mocks;
 global using DiscountGrpc.Models;
+global using DiscountGrpc.Protos;
+global using Google.Protobuf.WellKnownTypes;
+global using Grpc.Core;
+global using Mapster;
