220e1484d4c91159ea8ee0105c13da75358af9dc
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2022-08-15 20:42:47 +0430

    handle null discount

--- ShoppingCartGrpc/Factories/DiscountFactory.cs
+++ ShoppingCartGrpc/Factories/DiscountFactory.cs
@@ -13,6 +13,6 @@ public class DiscountFactory
     {
         var discount = await _client.GetDiscountAsync(new GetDiscountRequest() {Discount = discountCode});
 
-        return discount.Amount;
+        return discount?.Amount ?? 0;
     }
 }
