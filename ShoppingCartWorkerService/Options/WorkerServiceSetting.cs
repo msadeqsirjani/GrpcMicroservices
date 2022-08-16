43a29bd232286652db4272e8750a3a56ee1a688d
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2022-08-16 21:35:44 +0430

    add new setting into app setting

--- ShoppingCartWorkerService/Options/WorkerServiceSetting.cs
+++ ShoppingCartWorkerService/Options/WorkerServiceSetting.cs
@@ -6,4 +6,8 @@ public class WorkerServiceSetting
     public string Username { get; set; } = null!;
     public string ProductUrl { get; set; } = null!;
     public string ShoppingCartUrl { get; set; } = null!;
+    public string IdentityUrl { get; set; } = null!;
+    public string ClientId { get; set; } = null!;
+    public string ClientSecret { get; set; } = null!;
+    public string Scope { get; set; } = null!;
 }
